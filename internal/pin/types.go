8da4cea0f197277297420eb313d9ccd0f7016f81
yseto <yseto@users.noreply.github.com>
2023-07-09 17:07:36 +0000

    internal/pin support to JSON, sql/driver.Value

--- internal/pin/types.go
+++ internal/pin/types.go
@@ -1,5 +1,11 @@
 package pin
 
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+)
+
 type ReadFlag string
 
 const (
@@ -7,3 +13,54 @@ const (
 	Seen   ReadFlag = "Seen"
 	Setpin ReadFlag = "Setpin"
 )
+
+func (c ReadFlag) String() string {
+	switch c {
+	case Unseen:
+		return "Unseen"
+	case Seen:
+		return "Seen"
+	case Setpin:
+		return "Setpin"
+	default:
+		panic("Unknown")
+	}
+}
+
+// for client response.
+func (c ReadFlag) MarshalJSON() ([]byte, error) {
+	return json.Marshal(c.String())
+}
+
+// for write to db
+func (c ReadFlag) Value() (driver.Value, error) {
+	switch c {
+	case Unseen:
+		return int64(0), nil
+	case Seen:
+		return int64(1), nil
+	case Setpin:
+		return int64(2), nil
+	}
+	return int64(0), errors.New("incompatible value for ReadFlag")
+}
+
+// for read from db
+func (j *ReadFlag) Scan(src interface{}) error {
+	v, ok := src.(int64)
+	if !ok {
+		return errors.New("incompatible type for ReadFlag")
+	}
+
+	switch v {
+	case 0:
+		*j = "Unseen"
+	case 1:
+		*j = "Seen"
+	case 2:
+		*j = "Setpin"
+	default:
+		return errors.New("incompatible type for ReadFlag")
+	}
+	return nil
+}
