46f54c4fb5350a8ae7367109aec6f872ae93922f
yseto <yseto@users.noreply.github.com>
2023-07-09 14:46:31 +0000

    userid, db connection via context.Context

--- app/handler/context.go
+++ app/handler/context.go
@@ -2,12 +2,42 @@ package handler
 
 import (
 	"github.com/golang-jwt/jwt"
+	"context"
+
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/yseto/gion-go/db"
 )
+
+type userID struct{}
+
+func NewUserContext(ctx context.Context, userid uint64) context.Context {
+	return context.WithValue(ctx, userID{}, userid)
+}
+
+func userFromContext(ctx context.Context) uint64 {
+	return ctx.Value(userID{}).(uint64)
+}
+
+type dbConnKey struct{}
+
+func NewDBContext(ctx context.Context, h *sqlx.DB) context.Context {
+	return context.WithValue(ctx, dbConnKey{}, h)
+}
+
+func dbFromContext(ctx context.Context) *sqlx.DB {
+	return ctx.Value(dbConnKey{}).(*sqlx.DB)
+}
+
+func DBCommonFromContext(ctx context.Context) db.DB {
+	return db.New(dbFromContext(ctx))
+}
+
+func DBUserFromContext(ctx context.Context) db.UserScopedDB {
+	return db.NewUserScopedDB(dbFromContext(ctx), userFromContext(ctx))
+}
 
 type CustomContext struct {
 	echo.Context
