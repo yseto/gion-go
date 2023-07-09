129e3238084c762e1ad167a795cb309d3806c0ee
yseto <yseto@users.noreply.github.com>
2023-07-09 14:47:24 +0000

    remove extension of echo.Context

--- app/handler/context.go
+++ app/handler/context.go
@@ -1,12 +1,9 @@
 package handler
 
 import (
-	"github.com/golang-jwt/jwt"
 	"context"
 
 	"github.com/jmoiron/sqlx"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/yseto/gion-go/db"
 )
@@ -37,30 +34,4 @@ func DBCommonFromContext(ctx context.Context) db.DB {
 
 func DBUserFromContext(ctx context.Context) db.UserScopedDB {
 	return db.NewUserScopedDB(dbFromContext(ctx), userFromContext(ctx))
-}
-
-type CustomContext struct {
-	echo.Context
-	Conn *sqlx.DB
-}
-
-func (c *CustomContext) DBCommon() db.DB {
-	return db.New(c.Conn)
-}
-
-func (c *CustomContext) DBUser() db.UserScopedDB {
-	user := c.Get(SessionContextKey).(*jwt.Token)
-	claims := user.Claims.(*jwtCustomClaims)
-	return db.NewUserScopedDB(c.Conn, claims.UserID)
-}
-
-const SessionContextKey = "user"
-const JwtSignedKey = "secret"
-
-func JWTConfig() middleware.JWTConfig {
-	return middleware.JWTConfig{
-		Claims:     &jwtCustomClaims{},
-		SigningKey: []byte(JwtSignedKey),
-		ContextKey: SessionContextKey,
-	}
 }
